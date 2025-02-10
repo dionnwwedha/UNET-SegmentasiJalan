840fb241d969bcb6e1cf26b15d5b401f05ac4632
dionnwwedha <dionysius.swe2003@mail.ugm.ac.id>
2025-02-10 08:34:13 +0700

    update

---
+++ peta/data/aoi.js
@@ -0,0 +1,9 @@
+var aoi = 
+{
+"type": "FeatureCollection",
+"name": "AOI_AB",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 106.897214405052864, -6.58462541657389 ], [ 106.852863357569277, -6.584791915607909 ], [ 106.852776313764309, -6.561284913807285 ], [ 106.897125280735452, -6.561119014187356 ], [ 106.897214405052864, -6.58462541657389 ] ] ] ] } }
+]
+}
